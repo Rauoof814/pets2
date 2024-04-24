f3fb3275cd63af0252ef427c1971e4feb087a28a
Abdul Rahmani <rahmani.abdul@student.greenriver.edu>
2024-04-24 18:38:01 -0400

    a stylesheet form was created, pet-order page was created

--- index.php
+++ index.php
@@ -24,5 +24,12 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
+$f3->route('GET /order', function ()
+{
+
+    $view = new Template();
+    echo $view->render('views/pet-order.html');
+});
+
 // Run Fat-Free
 $f3->run();
