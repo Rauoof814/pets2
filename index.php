7189a32e1a9752620a4ac785f928986de6a21c75
Abdul Rahmani <rahmani.abdul@student.greenriver.edu>
2024-04-26 00:48:52 -0400

    index.php was modified and a summary route was created, order-summary

--- index.php
+++ index.php
@@ -24,12 +24,61 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET /order', function ()
+
+$f3->route('GET|POST /order', function ($f3)
 {
+
+    // Check if the for has been posted
+//    if ($_SERVER['REQUEST METHOD'] == 'POST')
+//        {
+//            echo "'POST' method was used ;)";
+//        }
+//    else
+//    {
+//        echo "'GET' method was used :)";
+//    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+
+
+        // Get the data
+        $pet = $_POST['pet'];
+        $color = $_POST['color'];
+
+        // Validate the data
+        if (empty($pet))
+        {
+            // Data is invalid
+            echo "Please supply a pet type :)";
+        }
+        else
+        {
+            // Data is valid
+            $f3->set('SESSION.pet', $pet);
+            $f3->set('SESSION.color', $color);
+
+            //**** Add the color of the session
+            //Redirect to the summary route
+            $f3->reroute("summary");
+        }
+    }
 
     $view = new Template();
     echo $view->render('views/pet-order.html');
 });
+
+
+
+// Order Summary
+$f3->route('GET /summary', function () {
+
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order-summary.html');
+});
+
 
 // Run Fat-Free
 $f3->run();
